0e356e85e1933c7a5a85a46d4b6b87aee7e7498f
aakansha meena <aakansha meena>
2016-07-10 04:54:05 +0530

    first commit

---
+++ app/src/main/java/com/example/harimohan/jsonparsing/JSONParser.java
@@ -0,0 +1,63 @@
+package com.example.harimohan.jsonparsing;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by HARI MOHAN on 05-Jul-16.
+ */
+public class JSONParser {
+
+    static InputStream is= null;
+    static JSONObject jObj= null;
+    static String json = "";
+
+    public JSONParser(){
+        //its a constructor
+    }
+
+    public JSONObject getJSONFromURL(String url){
+        try {
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }catch (ClientProtocolException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while((line = reader.readLine())!=null){
+                sb.append(line+"n");
+            }
+            is.close();
+            json =sb.toString();
+        }catch (Exception e){
+            Log.e("JSON Parser","Error parsing data"+e.toString());
+        }
+            return jObj;
+
+        }
+    }
+
